02aa3ee9f05905589f538eb717ccc7277ff2bfa1
joshua-miles <joshua@christetulit.org>
2019-04-24 13:05:36 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -3,24 +3,91 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <List/>
   );
 }
+let Tasks = [
+  { 
+    id: 1,
+    content: "buy groceries",
+    completed: false
+  }, 
+  { 
+    id: 2,
+    content: "drink some tea",
+    completed: true
+  }, 
+  { 
+    id: 3,
+    content: "finish this app",
+    completed: false
+  }, 
+  { 
+    id: 4,
+    content: "make dinner",
+    completed: false
+  },
+  { 
+    id: 5,
+    content: "make dinner now",
+    completed: false
+  }
+]
+
+class List extends React.Component{
+  
+  state = {
+    tasks: Tasks
+  }
+
+  render(){
+    return (
+      <ul>
+        {this.state.tasks.map( (task) => (
+          <Task key={task.id} id= {task.id} content={task.content} completed={task.completed} toggleCheck= {this.toggleCheck}/>
+        ))}
+      </ul>
+
+    )
+  }
+
+  toggleCheck = (id) => {
+    this.setState({
+      tasks: this.state.tasks.map((task)=>{
+        if (task.id === id ){
+          return { ...task, completed: !task.completed }
+        } else {
+          return task
+        }
+      })
+    })
+  }
+
+  
+
+  // Also works:
+  // constructor(){
+  //   this.state = {
+  //     tasks: Tasks
+  //   }
+  // }
+
+
+
+}
+
+class Task extends React.Component {
+
+    checkClicked = () => {
+      this.props.toggleCheck(this.props.id)
+    }
+
+    render(){
+      return <li><input type='checkbox' checked={this.props.completed} onChange={this.checkClicked}
+      />{this.props.content}</li>
+    }
+}
+
 
 export default App;
